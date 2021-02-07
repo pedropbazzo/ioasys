34e7f44f4dc38089d7d24a750f4dee5c5aaeea14
pedropbazzo <pedropbazzo@outlook.com>
2021-02-07 10:11:09 -0300

    Add: feat -d config app

--- src/App.js
+++ src/App.js
@@ -1,32 +1,23 @@
 import React from 'react';
 import {Image, StatusBar} from 'react-native';
-import {PersistGate} from 'redux-persist/integration/react';
-import {Provider} from 'react-redux';
-import {NavigationContainer} from '@react-navigation/native';
 
-import './config/ReactotronConfig';
+import {NavigationContainer} from '@react-navigation/native';
 
 import AppProvider from './hooks';
 import Routes from './routes/index';
 
-import {store, persistor} from './store';
-
 const App = () => {
   return (
     <NavigationContainer>
-      <Provider store={store}>
-        <PersistGate persistor={persistor}>
-          <StatusBar
-            barStyle="light-content"
-            backgroundColor="#312e38"
-            hidden
-            translucent
-          />
-          <AppProvider>
-            <Routes />
-          </AppProvider>
-        </PersistGate>
-      </Provider>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="#312e38"
+        hidden
+        translucent
+      />
+      <AppProvider>
+        <Routes />
+      </AppProvider>
     </NavigationContainer>
   );
 };
